4ed871aac9b01b6dbbb627a6805c0b0b16b4e5ad
Nizar <muhammadniyar282@gmail.com>
2026-02-09 15:07:36 +0700

    Fix favicon

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+            <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
+            <link rel="icon" type="image/png" href="/favicon.png?v=2" />
+            <link rel="shortcut icon" href="/favicon.ico?v=2" />
+            <link rel="apple-touch-icon" href="/favicon.png?v=2" />
+        </>
+    )
+}
